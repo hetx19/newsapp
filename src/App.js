b7471b055ae9cd830b10fb3a01dedae8f583439f
Harshal Savaliya <hetsavaliya19@gmail.com>
2022-09-11 20:14:54 +0530

    Removing unwanted files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Navbar from "./Components/Navbar";
+import News from "./Components/News";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import LoadingBar from "react-top-loading-bar";
 
 function App() {
+
+  const pageSize = 6
+  const apikey = process.env.REACT_APP_NEWS_API
+  const [progress, setProgress] = useState(0)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Navbar />
+        <LoadingBar
+          height={3}
+          color='#f11946'
+          progress={progress}
+        />
+        <Switch>
+          <Route exact path="/">
+            <News key="general" setProgress={setProgress} apikey={apikey} pageSize={pageSize} country="in" category="general" />
+          </Route>
+          <Route exact path="/business">
+            <News key="business" setProgress={setProgress} apikey={apikey} pageSize={pageSize} country="in" category="business" />
+          </Route>
+          <Route exact path="/entertainment">
+            <News key="entertainment" setProgress={setProgress} apikey={apikey} pageSize={pageSize} country="in" category="entertainment" />
+          </Route>
+          <Route exact path="/general">
+            <News key="general" setProgress={setProgress} apikey={apikey} pageSize={pageSize} country="in" category="general" />
+          </Route>
+          <Route exact path="/health">
+            <News key="health" setProgress={setProgress} apikey={apikey} pageSize={pageSize} country="in" category="health" />
+          </Route>
+          <Route exact path="/science">
+            <News key="science" setProgress={setProgress} apikey={apikey} pageSize={pageSize} country="in" category="science" />
+          </Route>
+          <Route exact path="/sports">
+            <News key="sports" setProgress={setProgress} apikey={apikey} pageSize={pageSize} country="in" category="sports" />
+          </Route>
+          <Route exact path="/technology">
+            <News key="technology" setProgress={setProgress} apikey={apikey} pageSize={pageSize} country="in" category="technology" />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
